105f26bf7cae404c94b3bd803de38ba2c9f11ac2
anandkumarb2018 <anandkumarb@virtusa.com>
2019-03-14 19:47:09 +0530

    removed unnecessary REST APIs

--- src/main/java/com/mufg/mufghelloworld/MufgHelloWorldApplication.java
+++ src/main/java/com/mufg/mufghelloworld/MufgHelloWorldApplication.java
@@ -2,7 +2,11 @@ package com.mufg.mufghelloworld;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
@@ -13,4 +17,11 @@ public class MufgHelloWorldApplication {
 		SpringApplication.run(MufgHelloWorldApplication.class, args);
 	}
 
+	@Bean
+    public Docket api() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .select()
+                .apis(RequestHandlerSelectors.basePackage("com.mufg.mufghelloworld.controller"))
+                .build();
+    }
 }
